88e09e71f547e1df2ff8a70779f238d5e85a039d
Serhii Ilin <serhii.ilin@semanticbits.com>
2021-09-29 15:10:25 -0400

    cql syntax

--- src/AceEditor/madie-ace-editor.tsx
+++ src/AceEditor/madie-ace-editor.tsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import AceEditor from "react-ace";
 
-import "ace-builds/src-noconflict/mode-java";
-import "ace-builds/src-noconflict/theme-monokai";
-
+// import * as Ace from "ace-builds"
+// import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/theme-eclipse";
+import CqlMode from "./cql-mode";
+// import "ace-builds/src-noconflict/mode-python";
+// import "ace-builds/src-noconflict/mode-cql";
 export default function MadieAceEditor({ props }) {
+  const aceRef = useRef<AceEditor>(null);
+
+  useEffect(() => {
+    const cqlMode = new CqlMode();
+    // @ts-ignore
+    aceRef?.current?.editor?.getSession()?.setMode(cqlMode);
+  }, []);
+
+  // useMemo(() => {
+  //   // @ts-ignore
+  //   aceRef?.current?.editor?.getSession()?.setMode(CqlMode);
+  // }, []);
+
   return (
     <AceEditor
-      mode="java"
-      theme="monokai"
+      // mode="java"
+      ref={aceRef}
+      theme="eclipse"
       defaultValue={props.defaultValue}
       onChange={(val) => {
         props.handleValueChanges(val);
