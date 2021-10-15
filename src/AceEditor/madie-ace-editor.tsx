64520f5366371adcc3944e05aed5a1d9107b16bc
Serhii Ilin <serhii.ilin@semanticbits.com>
2021-10-15 13:37:09 -0400

    cql syntax cleanup (#23)
    * cql syntax cleanup
    
    * cql syntax

--- src/AceEditor/madie-ace-editor.tsx
+++ src/AceEditor/madie-ace-editor.tsx
@@ -8,15 +8,11 @@ import CqlMode from "./cql-mode";
 
 export default function MadieAceEditor({ props }) {
   const aceRef = useRef<AceEditor>(null);
-  const localStorageKey = props.localStorageKey ?? "cql";
-  const [cql, setCql] = React.useState(localStorage.getItem(localStorageKey));
-
   useEffect(() => {
     const cqlMode = new CqlMode();
     // @ts-ignore
     aceRef?.current?.editor?.getSession()?.setMode(cqlMode);
-    localStorage.setItem(localStorageKey, cql);
-  }, [cql, localStorageKey]);
+  }, []);
 
   return (
     <AceEditor
@@ -25,13 +21,11 @@ export default function MadieAceEditor({ props }) {
       theme="monokai"
       defaultValue={props.defaultValue}
       onChange={(val) => {
-        setCql(aceRef?.current?.editor?.getValue());
         props.handleValueChanges(val);
       }}
       width="100%"
       name="ace-editor-wrapper"
       enableBasicAutocompletion={true}
-      value={cql}
       editorProps={{ $blockScrolling: true }}
     />
   );
