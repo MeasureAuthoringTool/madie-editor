f2d18ab9942cea6d9955de1a697a353d0382d760
Prateek Keerthi <prateekkeerthi7@gmail.com>
2024-04-29 12:15:31 -0400

    MAT-6855 added pagination

--- src/terminologySectionPanel/codesSection/codesSubSection/savedCodesSubSection/SavedCodesSubSection.tsx
+++ src/terminologySectionPanel/codesSection/codesSubSection/savedCodesSubSection/SavedCodesSubSection.tsx
@@ -1,4 +1,6 @@
-import React, { useMemo, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import queryString from "query-string";
 import TerminologySection from "../../../../common/TerminologySection";
 import {
   ColumnDef,
@@ -8,6 +10,7 @@ import {
 } from "@tanstack/react-table";
 import tw from "twin.macro";
 import "styled-components/macro";
+import { Pagination } from "@madie/madie-design-system/dist/react";
 
 export default function SavedCodesSubSection() {
   type TCRow = {
@@ -18,6 +21,54 @@ export default function SavedCodesSubSection() {
   };
 
   const [data, setData] = useState<TCRow[]>([]);
+
+  //currently we are using random data numbers
+  // TODO: integrate with actual data
+  const [totalPages, setTotalPages] = useState<number>(0);
+  const [totalItems, setTotalItems] = useState<number>(5);
+  const [visibleItems, setVisibleItems] = useState<number>(0);
+
+  const [offset, setOffset] = useState<number>(0);
+  const [currentLimit, setCurrentLimit] = useState<number>(5);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const canGoPrev = currentPage > 1;
+  const canGoNext = (() => {
+    return currentPage < totalPages;
+  })();
+  const handlePageChange = (e, v) => {
+    setCurrentPage(v);
+  };
+  const handleLimitChange = (e) => {
+    setCurrentLimit(e.target.value);
+    setCurrentPage(0);
+  };
+
+  useEffect(() => {
+    managePagination();
+  }, [currentPage, currentLimit]);
+
+  const managePagination = useCallback(() => {
+    if (totalItems < currentLimit) {
+      setOffset(0);
+      setVisibleItems(totalItems);
+      setTotalItems(totalItems);
+      setTotalPages(1);
+    } else {
+      const start = (currentPage - 1) * currentLimit;
+      setOffset(start);
+      setVisibleItems(currentLimit);
+      setTotalItems(totalItems);
+      setTotalPages(Math.ceil(totalItems / currentLimit));
+    }
+  }, [
+    currentLimit,
+    currentPage,
+    setOffset,
+    setVisibleItems,
+    setTotalItems,
+    setTotalPages,
+  ]);
 
   const TH = tw.th`p-3 text-left text-sm font-bold capitalize`;
   const columns = useMemo<ColumnDef<TCRow>[]>(
@@ -57,7 +108,7 @@ export default function SavedCodesSubSection() {
   });
 
   return (
-    <div style={{ marginTop: "30px" }}>
+    <div>
       <TerminologySection
         title="Saved Codes"
         children={
@@ -88,6 +139,22 @@ export default function SavedCodesSubSection() {
           </table>
         }
       />
+      <div className="pagination-container">
+        <Pagination
+          totalItems={totalItems}
+          visibleItems={visibleItems}
+          limitOptions={[5, 10, 25, 50]}
+          offset={offset}
+          handlePageChange={handlePageChange}
+          handleLimitChange={handleLimitChange}
+          page={currentPage}
+          limit={currentLimit}
+          count={totalPages}
+          shape="rounded"
+          hideNextButton={!canGoNext}
+          hidePrevButton={!canGoPrev}
+        />
+      </div>
     </div>
   );
 }
