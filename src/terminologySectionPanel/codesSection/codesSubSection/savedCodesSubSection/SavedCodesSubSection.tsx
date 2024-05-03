db66f746845847fbb9ec8692967a5351514e29c3
Prateek Keerthi <prateekkeerthi7@gmail.com>
2024-05-03 13:01:36 -0400

    MAT-6856 fixed lint issue

--- src/terminologySectionPanel/codesSection/codesSubSection/savedCodesSubSection/SavedCodesSubSection.tsx
+++ src/terminologySectionPanel/codesSection/codesSubSection/savedCodesSubSection/SavedCodesSubSection.tsx
@@ -67,6 +67,25 @@ export default function SavedCodesSubSection({ measureStoreCql }) {
     managePagination();
   }, [currentPage, currentLimit]);
 
+  //loading of codes
+  useEffect(() => {
+    if (measureStoreCql) {
+      setLoading(true);
+      const parsedCql = new CqlAntlr(measureStoreCql).parse();
+      if (parsedCql.codes) {
+        const codesList = parsedCql.codes.map((code) => {
+          const { codeId, codeSystem } = code;
+          return {
+            code: codeId.replace(/['"]+/g, ""),
+            codeSystem: codeSystem.replace(/['"]+/g, ""),
+            version: "",
+          };
+        });
+        RetrieveCodeDetailsList(codesList);
+      }
+    }
+  }, [measureStoreCql]);
+
   const managePagination = useCallback(() => {
     if (totalItems < currentLimit) {
       setOffset(0);
@@ -126,45 +145,26 @@ export default function SavedCodesSubSection({ measureStoreCql }) {
     getCoreRowModel: getCoreRowModel(),
   });
 
-  //loading of codes
-  useEffect(() => {
-    if (measureStoreCql) {
-      RetrieveCodeDetailsList(measureStoreCql);
-    }
-  }, [measureStoreCql]);
-
-  const RetrieveCodeDetailsList = async (editorValue) => {
-    setLoading(true);
-    const parsedCql = new CqlAntlr(editorValue).parse();
-    if (parsedCql.codes) {
-      const codesList = parsedCql.codes.map((code) => {
-        const { codeId, codeSystem } = code;
-        return {
-          code: codeId.replace(/['"]+/g, ""),
-          codeSystem: codeSystem.replace(/['"]+/g, ""),
-          version: "",
-        };
+  const RetrieveCodeDetailsList = async (codesList) => {
+    const terminologyService = await useTerminologyServiceApi();
+    terminologyService
+      .getCodeDetailsList(codesList)
+      .then((response) => {
+        setData(response.data.filter((code) => code !== null));
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+        if (error.response?.status === 404) {
+          setData(undefined);
+        } else {
+          console.error(error);
+          setToastMessage(
+            "An issue occurred while retrieving the code from VSAC. Please try again. If the issue continues, please contact helpdesk."
+          );
+          setToastOpen(true);
+        }
       });
-      const terminologyService = await useTerminologyServiceApi();
-      terminologyService
-        .getCodeDetailsList(codesList)
-        .then((response) => {
-          setData(response.data.filter((code) => code !== null));
-          setLoading(false);
-        })
-        .catch((error) => {
-          setLoading(false);
-          if (error.response?.status === 404) {
-            setData(undefined);
-          } else {
-            console.error(error);
-            setToastMessage(
-              "An issue occurred while retrieving the code from VSAC. Please try again. If the issue continues, please contact helpdesk."
-            );
-            setToastOpen(true);
-          }
-        });
-    }
   };
 
   const getCodeStatus = (status) => {
