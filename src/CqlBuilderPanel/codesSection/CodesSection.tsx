41b68e8caf322c3787f518647ad971619fb0dc63
Cecilia Liu <cecilia.liu@icf.com>
2024-07-23 08:49:29 -0500

    MAT-7414 add number of saved codes to tab

--- src/CqlBuilderPanel/codesSection/CodesSection.tsx
+++ src/CqlBuilderPanel/codesSection/CodesSection.tsx
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "twin.macro";
 import "styled-components/macro";
 import CodesSectionNavTabs from "./CodesSectionNavTabs";
 import CodeSubSection from "./codesSubSection/codeSubSection/CodeSubSection";
-import SavedCodesSubSection from "./codesSubSection/savedCodesSubSection/SavedCodesSubSection";
+import SavedCodesSubSection, {
+  getCodeVersion,
+  getCodeSuffix,
+  CodesList,
+} from "./codesSubSection/savedCodesSubSection/SavedCodesSubSection";
 import { useCodeSystems } from "./useCodeSystems";
 import { CqlMetaData } from "../../api/useTerminologyServiceApi";
+import { CqlAntlr } from "@madie/cql-antlr-parser/dist/src";
+import _ from "lodash";
 
 interface CodesSectionProps {
   canEdit: boolean;
@@ -32,9 +38,48 @@ export default function CodesSection({
 }: CodesSectionProps) {
   const [activeTab, setActiveTab] = useState<string>("code");
   const { codeSystems } = useCodeSystems();
+  const [parsedCodesList, setParsedCodesList] = useState<CodesList[]>(null);
+
+  useEffect(() => {
+    if (measureStoreCql) {
+      const parsedCql = new CqlAntlr(measureStoreCql).parse();
+      if (!_.isEmpty(parsedCql?.codes)) {
+        const codesList = parsedCql.codes.map((code) => {
+          const matchedCodeSystem = parsedCql.codeSystems.find(
+            (codeSystem) =>
+              codeSystem.name?.replace(/['"]+/g, "") ===
+              code.codeSystem?.replace(/['"]+/g, "")
+          );
+          const parsedCode = code.codeId.replace(/['"]+/g, "");
+          const parsedCodeSystem = code.codeSystem.replace(/['"]+/g, "");
+          const codeSystemVersion = getCodeVersion(
+            parsedCode,
+            parsedCodeSystem,
+            matchedCodeSystem?.oid,
+            cqlMetaData?.codeSystemMap,
+            matchedCodeSystem?.version
+          );
+          return {
+            code: parsedCode,
+            codeSystem: parsedCodeSystem.replace(":" + codeSystemVersion, ""),
+            version: codeSystemVersion,
+            oid: matchedCodeSystem?.oid,
+            suffix: getCodeSuffix(code),
+            versionIncluded: code.codeSystem.includes(codeSystemVersion),
+          };
+        });
+        setParsedCodesList(codesList);
+      }
+    }
+  }, [measureStoreCql]);
+
   return (
     <>
-      <CodesSectionNavTabs activeTab={activeTab} setActiveTab={setActiveTab} />
+      <CodesSectionNavTabs
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+        savedCodes={parsedCodesList ? parsedCodesList?.length : 0}
+      />
       <div tw="mt-4">
         {activeTab === "code" && (
           <CodeSubSection
