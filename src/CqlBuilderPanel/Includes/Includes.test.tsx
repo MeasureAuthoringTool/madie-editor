c83cfd4d8c3a06fdc0c62c57f4957a2a38255562
adongare <ashok.dongare@semanticbits.com>
2024-08-13 11:38:45 -0400

    MAT-7462 fix test case

--- src/CqlBuilderPanel/Includes/Includes.test.tsx
+++ src/CqlBuilderPanel/Includes/Includes.test.tsx
@@ -1,14 +1,14 @@
 import * as React from "react";
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Includes from "./Includes";
 
 const { getByTestId } = screen;
 
 describe("Includes", () => {
-  it("renders Includes", async () => {
+  it("Should renders Includes component", async () => {
     render(<Includes canEdit measureModel={"QDM"} />);
     expect(getByTestId("includes-panel")).toBeInTheDocument();
 
-    expect(getByTestId("search-text-input")).toBeEnabled();
+    expect(getByTestId("searchTerm-text-input")).toBeEnabled();
   });
 });
