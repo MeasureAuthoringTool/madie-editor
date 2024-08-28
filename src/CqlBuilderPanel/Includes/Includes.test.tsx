0c6e3be7175214b3992c516a98053b1c135909af
adongare <ashok.dongare@semanticbits.com>
2024-08-28 12:34:34 -0400

    MAT-7465 more test cases

--- src/CqlBuilderPanel/Includes/Includes.test.tsx
+++ src/CqlBuilderPanel/Includes/Includes.test.tsx
@@ -1,16 +1,36 @@
 import * as React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import Includes from "./Includes";
+import userEvent from "@testing-library/user-event";
 
-const { getByTestId } = screen;
+const { getByTestId, getByRole } = screen;
+const cql =
+  "library CaseWhenThen version '0.3.000'\n" +
+  "using QDM version '5.6'\n" +
+  "include CancerLinQ version '1.5.000' called CancerLinQQ";
 
 describe("Includes", () => {
   it("Should renders Includes component", async () => {
     render(
-      <Includes canEdit measureModel={"QDM"} handleApplyLibrary={jest.fn} />
+      <Includes
+        canEdit
+        measureModel={"QDM"}
+        handleApplyLibrary={jest.fn}
+        cql={cql}
+      />
     );
     expect(getByTestId("includes-panel")).toBeInTheDocument();
-
     expect(getByTestId("searchTerm-text-input")).toBeEnabled();
+    // by default Library tab active
+    expect(getByRole("tab", { name: /Library/i })).toHaveAttribute(
+      "aria-selected"
+    );
+    // switch over to saved library tab
+    const savedLibraryTab = getByRole("tab", { name: /Saved Libraries/i });
+    userEvent.click(savedLibraryTab);
+    expect(savedLibraryTab).toHaveAttribute("aria-selected");
+    await waitFor(() => {
+      expect(savedLibraryTab).toHaveTextContent("Saved Libraries (1)");
+    });
   });
 });
