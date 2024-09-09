782651840431fb71d3e9f9d78c231a649e9e7b8e
mcmcphillips <matt.mcphillips@icf.com>
2024-09-09 14:16:50 -0700

    MAT-7443: Fix dependencies

--- src/AceEditor/madie-ace-editor.tsx
+++ src/AceEditor/madie-ace-editor.tsx
@@ -4,11 +4,14 @@ import * as _ from "lodash";
 import tw from "twin.macro";
 import { CqlAntlr } from "@madie/cql-antlr-parser/dist/src";
 
-import "ace-builds";
-import "ace-builds/webpack-resolver";
 import "ace-builds/src-noconflict/mode-sql";
 import "ace-builds/src-noconflict/theme-monokai";
 import "ace-builds/src-noconflict/ext-language_tools";
+import 'ace-builds/src-noconflict/ext-searchbox';
+
+const ace = require("ace-builds/src-noconflict/ace");
+ace.config.set('basePath', require('ace-builds').config.basePath);
+
 import CqlMode from "./cql-mode";
 import { Ace, Range } from "ace-builds";
 import CqlError from "@madie/cql-antlr-parser/dist/src/dto/CqlError";
