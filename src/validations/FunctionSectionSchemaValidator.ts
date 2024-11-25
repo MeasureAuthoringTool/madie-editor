0a97626e34a9439bf30f19f814ded550b9430bd6
Christopher Hubert <christopher.hubert@semanticbits.com>
2024-11-25 11:10:40 -0500

    MAT-7790: remove function name validation

--- src/validations/FunctionSectionSchemaValidator.ts
+++ src/validations/FunctionSectionSchemaValidator.ts
@@ -1,9 +1,7 @@
 import * as Yup from "yup";
 
 export const FunctionSectionSchemaValidator = Yup.object().shape({
-  functionName: Yup.string()
-    .required("Function name is required")
-    .matches(/^[a-zA-Z0-9]*$/, "No spaces or special characters are allowed"),
+  functionName: Yup.string().required("Function name is required"),
   comment: Yup.string(),
   fluentFunction: Yup.boolean(),
   body: Yup.string(),
