9c7508ba5ee132a8e92ba0d521ace516edf69d4b
nmorasb <nate.moraca@semanticbits.com>
2025-01-21 11:26:14 -0500

    MAT-7098: test updates

--- src/CqlBuilderPanel/common/ActionCenter.test.tsx
+++ src/CqlBuilderPanel/common/ActionCenter.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { logRoles, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom/extend-expect";
 import ActionCenter, { PropTypes, ActionItemDef } from "./ActionCenter";
@@ -15,56 +15,55 @@ const renderComponent = (props: Partial<PropTypes> = {}) => {
     actions: mockActions,
     idSuffix: "",
     ...props,
-  };
+  } as PropTypes;
   return render(<ActionCenter {...defaultProps} />);
 };
 
 describe("ActionCenter", () => {
   it("renders without crashing", () => {
-    const { getByTestId } = renderComponent();
-    expect(getByTestId("action-center")).toBeInTheDocument();
-  });
-
-  it("toggles open state on button click", () => {
-    const { getByTestId } = renderComponent();
-    const button = getByTestId("action-center-button");
-    userEvent.click(button);
-    expect(getByTestId("action-center")).toHaveStyle("background-color: white");
-    userEvent.click(button);
-    expect(getByTestId("action-center")).toHaveStyle(
-      "background-color: transparent"
-    );
+    renderComponent();
+    expect(screen.getByTestId("action-center")).toBeInTheDocument();
   });
 
   it("displays actions when open", () => {
-    const { getByTestId, queryByTestId } = renderComponent();
-    const button = getByTestId("action-center-button");
-    userEvent.click(button);
-    mockActions.forEach((action) => {
-      expect(getByTestId(action.name.replace(/\s/g, ""))).toBeInTheDocument();
+    renderComponent();
+    const button = screen.getByRole("button", {
+      name: "Measure action center",
     });
     userEvent.click(button);
     mockActions.forEach((action) => {
       expect(
-        queryByTestId(action.name.replace(/\s/g, ""))
+        screen.getByTestId("action-center_" + action.name.replace(/\s/g, ""))
+      ).toBeInTheDocument();
+    });
+    userEvent.click(button);
+    mockActions.forEach((action) => {
+      expect(
+        screen.queryByTestId("action-center_" + action.name.replace(/\s/g, ""))
       ).not.toBeInTheDocument();
     });
   });
 
-  it("calls action onClick and closes on action click", () => {
-    const { getByTestId } = renderComponent();
-    const button = getByTestId("action-center-button");
+  it("calls action onClick", () => {
+    renderComponent();
+    const button = screen.getByRole("button", {
+      name: "Measure action center",
+    });
     userEvent.click(button);
-    const actionButton = getByTestId(mockActions[0].name.replace(/\s/g, ""));
+    const actionButton = screen.getByTestId(
+      "action-center_" + mockActions[0].name.replace(/\s/g, "")
+    );
     userEvent.click(actionButton);
     expect(mockActions[0].onClick).toHaveBeenCalled();
   });
 
   it("applies idSuffix correctly", async () => {
-    const { getByTestId } = renderComponent({ idSuffix: "test" });
-    const button = getByTestId("action-center-button-test");
+    renderComponent({ idSuffix: "test" });
+    const button = screen.getByRole("button", {
+      name: "Measure action center",
+    });
     userEvent.click(button);
     expect(await screen.findByTestId("action-center-test")).toBeInTheDocument();
-    expect(getByTestId("action-center-button-test")).toBeInTheDocument();
+    expect(screen.getByTestId("action-center-button-test")).toBeInTheDocument();
   });
 });
