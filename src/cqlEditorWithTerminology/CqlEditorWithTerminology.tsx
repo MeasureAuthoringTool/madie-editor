bb2abbdeadc69f9800bff8b978155f00b4122ce5
Prateek Keerthi <prateekkeerthi7@gmail.com>
2026-03-17 14:52:53 -0400

    MAT-7023 fixed the hardcoding of expanded section to check params

--- src/cqlEditorWithTerminology/CqlEditorWithTerminology.tsx
+++ src/cqlEditorWithTerminology/CqlEditorWithTerminology.tsx
@@ -43,7 +43,11 @@ const CqlEditorWithTerminology = ({
   getCqlDefinitionReturnTypes,
   hasCqlError,
 }: EditorPropsType) => {
-  const [expanded, setExpanded] = useState(true);
+  const [expanded, setExpanded] = useState(() => {
+    const params = new URLSearchParams(window.location.search);
+    return !params.has("tab");
+  });
+
   const toggleSearch = () => {
     const event = new CustomEvent("toggleEditorSearchBox");
     window.dispatchEvent(event);
