aa9eb0b326b35aefb6099d400bf502aafb6c40d3
Prateek Keerthi <prateekkeerthi7@gmail.com>
2026-04-08 09:23:27 -0400

    fixed lint issue

--- src/api/useAIService.ts
+++ src/api/useAIService.ts
@@ -213,6 +213,7 @@ export class AIServiceApi {
         const decoder = new TextDecoder();
         let buffer = "";
 
+        // eslint-disable-next-line no-constant-condition
         while (true) {
           const { done, value } = await reader.read();
           if (done) break;
